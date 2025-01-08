0b84e88d70b01b32b4c583150d0edc34ed4518a5
dvbdbb <dovy1@hotmail.co.uk>
2025-01-08 14:43:11 +0000

    Basic board layout with tailwind

---
+++ frontend/src/components/Board.js
@@ -0,0 +1,9 @@
+const Board = ({ title }) => {
+    return (
+        <div className="w-1/4 h-full bg-blue-400 rounded-xl">
+            <h2 className="py-2 text-center text-white text-xl font-bold">{title}</h2>
+        </div>
+    );
+};
+
+export default Board;
