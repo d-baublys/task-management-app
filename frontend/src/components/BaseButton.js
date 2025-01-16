79712b052077bb2240593998c8bd60c432c11a3e
dvbdbb <dovy1@hotmail.co.uk>
2025-01-16 15:12:26 +0000

    Add delete confirm prompt, fix conditional button colours

--- frontend/src/components/BaseButton.js
+++ frontend/src/components/BaseButton.js
@@ -1,11 +1,13 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const BaseButton = ({ onClick, dropRef, icon }) => {
+const BaseButton = ({ onClick, dropRef, isOver, icon }) => {
     return (
         <button
             onClick={onClick}
             ref={dropRef}
-            className={`sticky flex justify-center items-center w-full rounded-2xl bg-slate-400`}
+            className={`sticky flex justify-center items-center w-full rounded-2xl  hover:bg-slate-300 ${
+                isOver ? "bg-slate-300" : "bg-slate-400"
+            }`}
             style={{ top: "var(--board-btn-top)", height: "var(--board-btn-spacing)" }}
         >
             <FontAwesomeIcon className="m-2 text-white size-8" icon={icon} />
--- frontend/src/components/DeleteButton.js
+++ frontend/src/components/DeleteButton.js
@@ -3,14 +3,22 @@ import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 import BaseButton from "./BaseButton";
 
 const DeleteButton = ({ deleteTask }) => {
+    const askConfirm = (item) => {
+        if (!window.confirm(`Are you sure you want to delete task "${item.id}"?`)) {
+            return;
+        }
+
+        deleteTask(item.id);
+    };
+
     const [{ isOver }, dropRef] = useDrop(() => ({
         accept: "BOX",
-        drop: (item) => deleteTask(item.id),
+        drop: (item) => askConfirm(item),
         collect: (monitor) => ({
             isOver: monitor.isOver(),
         }),
     }));
-    return <BaseButton dropRef={dropRef} icon={faTrashAlt} />;
+    return <BaseButton dropRef={dropRef} isOver={isOver} icon={faTrashAlt} />;
 };
 
 export default DeleteButton;
