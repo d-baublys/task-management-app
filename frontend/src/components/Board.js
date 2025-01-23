3da540c2dac7972eca95f1d8c247fb0e8ad1b656
dvbdbb <dovy1@hotmail.co.uk>
2025-01-23 12:51:35 +0000

    Use hover function for more dynamic movement between boards, separate board and tile dnd zones

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -1,20 +1,46 @@
 import { useDrop } from "react-dnd";
 import DraggableTile from "./DraggableTile";
-import { useCallback } from "react";
+import { useCallback, useRef } from "react";
 
 const Board = ({ title, titles, setTasks, boardTasks, reorderTasks, onDrop }) => {
+    const excludeRef = useRef(null);
+
     const [{ isOver }, dropRef] = useDrop(() => ({
         accept: "BOX",
-        drop: (item) => {
-            if (item.status !== titles[title]) {
+        drop: (item, monitor) => {
+            const clientOffset = monitor.getClientOffset();
+            if (!clientOffset) return;
+
+            const excludeBounding = excludeRef.current?.getBoundingClientRect();
+
+            const inDropZone =
+                clientOffset.x < excludeBounding.left ||
+                clientOffset.x > excludeBounding.right ||
+                clientOffset.y < excludeBounding.top ||
+                clientOffset.y > excludeBounding.bottom;
+
+            if (item.status === titles[title] && inDropZone) {
                 setTasks((prevTasks) => {
                     const dragIndex = prevTasks.findIndex((task) => task.id === item.id);
                     const updatedTasks = [...prevTasks];
                     const [movedTask] = updatedTasks.splice(dragIndex, 1);
-                    movedTask.status = titles[title]
 
                     updatedTasks.push(movedTask);
+                    console.log("hover end board");
+                    reorderTasks(updatedTasks);
 
+                    return updatedTasks;
+                });
+            }
+        },
+        hover: (item) => {
+            if (item.status !== titles[title]) {
+                item.status = titles[title];
+                setTasks((prevTasks) => {
+                    const dragIndex = prevTasks.findIndex((task) => task.id === item.id);
+                    const updatedTasks = [...prevTasks];
+                    updatedTasks[dragIndex].status = titles[title];
+                    console.log("hover move board");
                     reorderTasks(updatedTasks);
 
                     return updatedTasks;
@@ -37,7 +63,7 @@ const Board = ({ title, titles, setTasks, boardTasks, reorderTasks, onDrop }) =>
             const [movedTask] = updatedTasks.splice(dragIndex, 1);
 
             updatedTasks.splice(hoverIndex, 0, movedTask);
-
+            console.log("reorder hit");
             reorderTasks(updatedTasks);
 
             return updatedTasks;
@@ -50,7 +76,7 @@ const Board = ({ title, titles, setTasks, boardTasks, reorderTasks, onDrop }) =>
             ref={dropRef}
         >
             <h2 className="py-2 text-center text-white text-xl font-bold">{title}</h2>
-            <div className="flex flex-col gap-2 p-2">
+            <div ref={excludeRef} className="flex flex-col gap-2 px-2">
                 {boardTasks.map((tile, index) => (
                     <DraggableTile
                         key={tile.id}
