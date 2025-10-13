14a50c696bdf98582368bdc6e731cdc387a32e9d
dvbdbb <dovy1@hotmail.co.uk>
2025-10-13 12:26:35 +0100

    Add mode button animations

--- frontend/src/components/ModeButton.tsx
+++ frontend/src/components/ModeButton.tsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import useThemeContext from "../context/ThemeContext";
 import { IoMoon, IoSunny } from "react-icons/io5";
 import HeaderButton from "./base/HeaderButton";
+import { ThemeOptions } from "../lib/definitions";
 
 export default function ModeButton() {
     const { theme, toggleTheme } = useThemeContext();
+    const [isAnimated, setIsAnimated] = useState<boolean>(false);
+
+    const duration = 300;
+
+    const handleClick = () => {
+        if (isAnimated) return;
+
+        setIsAnimated(true);
+        toggleTheme();
+
+        setTimeout(() => {
+            setIsAnimated(false);
+        }, duration);
+    };
+
+    const animations = {
+        buttonBackground: `transition-colors duration-300 ease-in-out`,
+        iconOpacity: `transition-opacity duration-[450ms] ease-in-out`,
+        iconSpin: `animate-[spin_300ms_ease-in-out_1]`,
+    };
+
+    const generateIconClasses = (iconTheme: ThemeOptions) =>
+        `absolute ${animations["iconOpacity"]} ${
+            theme === iconTheme ? "opacity-100" : "opacity-0"
+        } ${iconTheme === "dark" ? "text-gray-mid" : "text-bare-base"}`;
 
     return (
-        <HeaderButton onClick={toggleTheme}>
+        <HeaderButton onClick={handleClick}>
             <div
-                className={`flex justify-center items-center aspect-square border border-black [border-radius:50%] p-1 cursor-pointer ${
-                    theme === "light" ? "bg-gray-mid" : "bg-bare-base"
+                className={`relative flex h-full justify-center items-center aspect-square border border-black [border-radius:50%] p-1 cursor-pointer ${
+                    animations["buttonBackground"]
+                } ${theme === "light" ? "bg-gray-mid" : "bg-bare-base"} ${
+                    isAnimated ? animations["iconSpin"] : ""
                 }`}
             >
-                {theme === "light" ? (
-                    <IoMoon className="h-full w-full text-bare-base" />
-                ) : (
-                    <IoSunny className="h-full w-full text-gray-mid" />
-                )}
+                <IoMoon className={generateIconClasses("light")} />
+                <IoSunny className={generateIconClasses("dark")} />
             </div>
         </HeaderButton>
     );
--- frontend/src/components/base/HeaderButton.tsx
+++ frontend/src/components/base/HeaderButton.tsx
@@ -6,7 +6,7 @@ export default function HeaderButton({ children, ...props }: HeaderButtonProps) 
     return (
         <button
             type="button"
-            className="relative flex shrink-0 items-center p-1 max-w-full bg-gray-300 dark:bg-gray-600 group hover:bg-gray-400 dark:hover:bg-gray-400 dark:text-gray-light hover:drop-shadow-md rounded-3xl theme-transition-all cursor-pointer z-20"
+            className="relative flex shrink-0 items-center p-1 max-w-full h-full bg-gray-300 dark:bg-gray-600 group hover:bg-gray-400 dark:hover:bg-gray-400 dark:text-gray-light hover:drop-shadow-md rounded-3xl theme-transition-all cursor-pointer z-20"
             {...props}
         >
             {children}
